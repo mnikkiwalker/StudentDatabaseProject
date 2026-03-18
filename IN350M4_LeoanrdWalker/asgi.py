c91aa8baa182fed6334746987e0cf80a6e8a8bd5
Nikki Walker <mnikkiwalker@gmail.com>
2026-03-18 19:36:21 -0400

    maybe found the last of the old project name

--- IN350M4_LeoanrdWalker/asgi.py
+++ IN350M4_LeoanrdWalker/asgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'IN350M4_LeoanrdWalker.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'StudentDatabaseProject.settings')
 
 application = get_asgi_application()
